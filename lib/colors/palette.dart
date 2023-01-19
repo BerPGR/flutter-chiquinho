ab692b458d0a0a8c5229a9f19a88df98796d4a18
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-01-19 17:47:25 -0300

    Adicionado imagens para guia e configuração inicial do projeto

---
+++ lib/colors/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color scaffoldBackground = Color(0xFFFFF6E7);
+  static const Color homeCard = Color(0xFF5F96E7);
+  static const Color homeCard2 = Color(0xFFF1B958);
+  static const Color homeCard3 = Color(0xFFEDA2A6);
+}
