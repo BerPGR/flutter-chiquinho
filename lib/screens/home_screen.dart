25f484ffff60bf4d4edf43d9951bb100501a4639
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-01-20 10:42:23 -0300

    Primeira parte da home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:chiquinho/widgets/famosos_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -6,6 +9,46 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Ola, usuario!',
+                          style: TextStyle(
+                              fontSize: 28, fontWeight: FontWeight.bold),
+                        ),
+                        Image.asset(
+                          'assets/images/logo.png',
+                          height: 50,
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Famosos do Chiquinho!",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SorvetesWidget()
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/famosos_widget.dart
@@ -0,0 +1,100 @@
+import 'package:chiquinho/colors/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SorvetesWidget extends StatelessWidget {
+  const SorvetesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Card(
+              elevation: 2,
+              color: Palette.homeCard3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 2,
+              color: Palette.homeCard3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Card(
+              elevation: 2,
+              color: Palette.homeCard3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 2,
+              color: Palette.homeCard3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Card(
+              elevation: 2,
+              color: Palette.homeCard3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 2,
+              color: Palette.homeCard3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
