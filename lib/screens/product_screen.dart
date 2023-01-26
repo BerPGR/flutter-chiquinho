5327e066bd94e83e58c2a0e5ed301f0a76213f8a
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-01-25 21:57:36 -0300

    Header da pagina de produtos

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:chiquinho/colors/palette.dart';
 import 'package:chiquinho/product/productBackground.dart';
 import 'package:flutter/material.dart';
 
@@ -23,13 +24,34 @@ class _ProductScreenState extends State<ProductScreen> {
       body: Stack(
         children: [
           ProductBackground(
-            screenHeight: MediaQuery.of(context).size.height,
+            screenHeight: MediaQuery.of(context).size.height * 0.5,
             colorId: widget.sorvete.categoriaId,
           ),
           SafeArea(
             child: SingleChildScrollView(
               child: Column(
-                children: [],
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(
+                                Icons.arrow_back,
+                                color: widget.sorvete.categoriaId == 3 ||
+                                        widget.sorvete.categoriaId == 4 ||
+                                        widget.sorvete.categoriaId == 5
+                                    ? Colors.black87
+                                    : Palette.scaffoldBackground,
+                                size: 30,
+                              ))
+                        ]),
+                  )
+                ],
               ),
             ),
           ),
